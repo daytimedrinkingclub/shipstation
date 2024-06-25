3fe4ffe7bacbe2a0bf7d0c4c485c0001e25ec072
Anuj Sharma <anujshama4u@gmail.com>
2024-06-25 17:10:09 +0530

    changes

--- server/services/s3Service.js
+++ server/services/s3Service.js
@@ -1,8 +1,10 @@
 const {
   PutObjectCommand,
   ListObjectsV2Command,
+  GetObjectCommand,
+  PutBucketPolicyCommand,
 } = require("@aws-sdk/client-s3");
-const { s3Client } = require("../config/awsConfig");
+const { s3Handler } = require("../config/awsConfig");
 require("dotenv").config();
 
 const bucketName = process.env.BUCKETEER_BUCKET_NAME;
@@ -14,7 +16,7 @@ async function saveFileToS3(bucketPath, content) {
     Body: content,
   };
   try {
-    await s3Client.send(new PutObjectCommand(params));
+    await s3Handler.send(new PutObjectCommand(params));
     console.log(`Successfully uploaded ${bucketPath}`);
   } catch (err) {
     console.error(`Error uploading ${bucketPath}:`, err);
@@ -28,9 +30,11 @@ async function listFoldersInS3(prefix) {
     Delimiter: "/",
   };
   try {
-    const data = await s3Client.send(new ListObjectsV2Command(params));
+    const data = await s3Handler.send(new ListObjectsV2Command(params));
     // Strip the prefix from each folder name
-    const folders = data.CommonPrefixes.map((item) => item.Prefix.replace(prefix, ''));
+    const folders = data.CommonPrefixes.map((item) =>
+      item.Prefix.replace(prefix, "")
+    );
     console.log(`Folders in '${prefix}':`, folders);
     return JSON.stringify(folders); // Return folders as a JSON array
   } catch (err) {
@@ -63,4 +67,49 @@ async function saveDirectoryToS3(directoryPath, remotePath) {
   }
 }
 
-module.exports = { saveFileToS3, saveDirectoryToS3, listFoldersInS3 };
+async function getFileFromS3(bucketPath) {
+  const params = {
+    Bucket: bucketName,
+    Key: bucketPath,
+  };
+  const data = await s3Handler.send(new GetObjectCommand(params));
+  console.log("data", data?.Body);
+  return data?.Body;
+}
+
+async function putBucketPolicy(bucketName, policy) {
+  const params = {
+    Bucket: bucketName,
+    Policy: JSON.stringify(policy),
+  };
+
+  const command = new PutBucketPolicyCommand(params);
+  try {
+    const data = await s3Handler.send(command);
+    console.log("Success", data);
+  } catch (error) {
+    console.error("Error", error);
+  }
+}
+
+const policy = {
+  Version: "2012-10-17",
+  Statement: [
+    {
+      Effect: "Allow",
+      Principal: "*",
+      Action: "s3:GetObject",
+      Resource: `arn:aws:s3:::${process.env.BUCKETEER_BUCKET_NAME}/*`,
+    },
+  ],
+};
+
+// Apply the policy
+// putBucketPolicy(process.env.BUCKETEER_BUCKET_NAME, policy);
+
+module.exports = {
+  saveFileToS3,
+  saveDirectoryToS3,
+  listFoldersInS3,
+  getFileFromS3,
+};
