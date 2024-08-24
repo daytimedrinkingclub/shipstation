79d47ec3f776c4c543b24372ae92c62e0a3131eb
Ashish Khare <akstyles211@gmail.com>
2024-08-25 00:38:58 +0530

    feat: prettier and lint-staged config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,17 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
-  { ignores: ["test/"] },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { ignores: ['test/'] },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   {
     rules: {
-      "no-unused-vars": "error",
-      "no-undef": "error",
+      'no-unused-vars': 'error',
+      'no-undef': 'error',
     },
   },
+  eslintConfigPrettier,
 ];
